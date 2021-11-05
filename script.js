0501d21bf646ea38ff6fed257815f82236238def
Washington Diniz <dinizwash@gmail.com>
2021-11-04 22:06:16 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function Dati(elemento){
+    const textox = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textox.forEach((letra, i)=>{
+        setTimeout(()=> elemento.innerHTML += letra, 90*i)
+        })
+    };
+
+const texto = document.querySelector('div.descricao p')
+Dati(texto);
+
+function onload(){
+    
+}
